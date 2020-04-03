cc0b7dddbcae1c37c5dfbe234820677e413f387c
Carson Brown <carson.mattox@gmail.com>
2020-04-03 16:19:48 -0700

    First commit.

---
+++ Code/unitTests.py
@@ -0,0 +1,9 @@
+import unittest
+
+class tests(unittest.TestCase):
+    # def testSpanInvalidType(self):
+    #     with self.assertRaises(TypeError):
+    #         span([DistortedInt(2, 5, 3), "DistortedInt(3, 5, 2)"])
+
+if __name__ == '__main__':
+    unittest.main()
