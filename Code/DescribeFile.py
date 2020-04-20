e9b330e2b6367f7bddc6829c7f6a034baffde0a9
Carson Brown <carson.mattox@gmail.com>
2020-04-20 14:18:17 -0700

    print top 10 cited sources

--- Code/DescribeFile.py
+++ Code/DescribeFile.py
@@ -8,16 +8,18 @@ def main():
         data = readFile(arg)
         if data is not None :
             arxivCitations(data)
-            print()
             avgCitatations(data)
+            mostCommonCitations(data)
 
+def indentDataFrame(toIndent):
+    return "\t" + toIndent.to_string().replace("\n", "\n\t") + "\n"
 
 def arxivCitations(data):
     arxivCitations = data.query("type == \"arxiv\"")
     onlyYears = arxivCitations["timestamp"].str.slice(stop=4)
     yearCounts = onlyYears.value_counts()
     print("Number Of ArXiv Citations By Year: ")
-    print("\t" + yearCounts.to_string().replace("\n", "\n\t"))
+    print(indentDataFrame(yearCounts))
 
 
 def avgCitatations(data):
@@ -26,8 +28,16 @@ def avgCitatations(data):
     median = valueCount.median()
     print("Average Citations:")
     print("\tMean:   " + str(mean))
-    print("\tMedian: " + str(median))
+    print("\tMedian: " + str(median) + "\n")
 
+def toUpper(s):
+    return s.upper()
+
+def mostCommonCitations(data):
+    print("The 10 Most Cited Sources Are: ")
+    citations = data["type"].apply(toUpper) + " " + data["id"]
+    citationCount = citations.value_counts().head(10)
+    print(indentDataFrame(citationCount))
 
 if __name__ == '__main__':
     main()
