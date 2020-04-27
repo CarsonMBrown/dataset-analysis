2381a5ecc116109fb9fcc486adc618810485c1e8
Weiye <60509897+wl44-li@users.noreply.github.com>
2020-04-27 14:31:27 +0100

    Zenodo analysis added

--- Code/analysis.py
+++ Code/analysis.py
@@ -10,14 +10,18 @@ def main():
             totalRecords(data)
             rangeOfDate(data)
             recordsByType(data)
-            recordsByPercentage(data) # Easy 1
+            recordsByPercentage(data)
+            zenodoByYear(data)
 
+            
 def indentDataFrame(toIndent):
     return "\t" + toIndent.to_string().replace("\n", "\n\t") + "\n"
 
+
 def totalRecords(data):
     print("Number of records:", data.shape[0], "\n")
-    
+   
+
 def rangeOfDate(data):
     date_data = pd.to_datetime(data['timestamp'])
     date_data = date_data.sort_values(ascending = True)
@@ -28,18 +32,33 @@ def rangeOfDate(data):
     print("Latest date (ns, UTC):", date_data.iloc[-1])
     print("Latest date:", date_data.iloc[-1].date(), "\n")
 
+    
 def recordsByType(data):
     counts = data['type'].value_counts()
     # use of value_counts that has been optimised for object type
-    table = pd.DataFrame({'Records count': counts})
+    table = counts.rename_axis('Record type').reset_index(name = 'Record count')
     print(table, "\n")
 
 
 def recordsByPercentage(data):
     percent = data['type'].value_counts(normalize = True).mul(100).round(1).astype(str)
-    table = pd.DataFrame({'percentage %': percent})
+    table = percent.rename_axis('Record type').reset_index(name = 'Percentage %')
     print(table, "\n")
     
+
+def zenodoByYear(data):
+    zenodo_data = data[data['id'].str.contains('zenodo', case = False)]
+    zenodo_data = pd.to_datetime(zenodo_data['timestamp']).dt.year
+    counts = zenodo_data.value_counts()
+    table = counts.rename_axis('Year').reset_index(name = 'Number of Zenodo cited')
+    
+    print("Sort by number of citations:\n")
+    print(table, "\n")
+
+    print("Sort by year of appearance:\n")
+    table = table.sort_values(table.columns[0])
+    print(table, "\n")
+
 
 if __name__ == '__main__':
     main()
