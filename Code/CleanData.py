2104f8e4f0bc77c9aab6065320ff5d3d263689d2
MohitWagh <mohitvwagh@gmail.com>
2020-05-02 03:39:45 +0400

    added CleanData.py

--- Code/CleanData.py
+++ Code/CleanData.py
@@ -5,11 +5,42 @@ import difflib
 
 
 def main():
+    for arg in sys.argv[1:]:
+        data = readFile(arg)
+        if data is not None:
+            checkNumberOfCols(data)
+            handleEmptyValues(data)
+            removeDuplicates(data)
 
+
+def checkNumberOfCols(data):
+    i = 0
+    for key in data.keys():
+        i = i + 1
+    if i == 6:
+        return True
+    print("Invalid number of columns")
+    return False
+
+
+def handleEmptyValues(data):
+    # Get columns with empty cells
+    num_of_empty_in_col = data.isnull().sum()
+    num_of_empty_in_col = num_of_empty_in_col[num_of_empty_in_col != 0]
+    if not num_of_empty_in_col.empty:
+        print("The following columns have empty fields")
+        print(num_of_empty_in_col)
+        for key in num_of_empty_in_col.keys():
+            if data[key].dtype == 'object':
+                data[key].fillna('*')
+            if data[key].dtype == 'int64':
+                data[key].dropna()
+
+def removeDuplicates(data):
+    data.drop_duplicates(data)
 
 if __name__ == '__main__':
     main()
-
 
 # def cleanInvalidLines(filename):
 #     """
