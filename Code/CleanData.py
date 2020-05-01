659bf81ca0043ec63e8217f3f4586d89ad346813
MohitWagh <mohitvwagh@gmail.com>
2020-05-02 02:17:44 +0400

    reinit CleanData.py

--- Code/CleanData.py
+++ Code/CleanData.py
@@ -3,32 +3,33 @@ import os
 from FileHelper import *
 import difflib
 
+
 def main():
-    for arg in sys.argv[1:] :
-        cleanInvalidLines(arg)
 
-def cleanInvalidLines(filename):
-    """
-    Removes all the invalid lines from the data.
-    """
-    filepath = "../Data/" + filename + ".tsv"
-    if fileValid(filepath) :
-        print("Cleaning Invalid Lines From: " + filepath)
-        file = open(filepath, "r", encoding='utf8')
-        checkTabs(file)
-
-def checkTabs(file):
-    """
-    Checks for lines that don't contain the correct number of tabs.
-    """
-    print("Checking Tabs...")
-    count = 0
-    for line in file.readlines():
-        line = line.strip()
-        numTabs = line.count("\t")
-        if numTabs != 5 :
-            print("\tInvalid Number Of Tabs On Line: ", count)
-        count = count + 1
 
 if __name__ == '__main__':
     main()
+
+
+# def cleanInvalidLines(filename):
+#     """
+#     Removes all the invalid lines from the data.
+#     """
+#     filepath = "../Data/" + filename + ".tsv"
+#     if fileValid(filepath) :
+#         print("Cleaning Invalid Lines From: " + filepath)
+#         file = open(filepath, "r", encoding='utf8')
+#         checkTabs(file)
+#
+# def checkTabs(file):
+#     """
+#     Checks for lines that don't contain the correct number of tabs.
+#     """
+#     print("Checking Tabs...")
+#     count = 0
+#     for line in file.readlines():
+#         line = line.strip()
+#         numTabs = line.count("\t")
+#         if numTabs != 5 :
+#             print("\tInvalid Number Of Tabs On Line: ", count)
+#         count = count + 1
